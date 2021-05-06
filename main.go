bf14b4d9be409865f03a26f60173bc72ccc77468
Joram Wambugu <artisan254@gmail.com>
2021-05-06 16:08:29 +0300

    STK Push
    Validate STK push request

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"gitlab.com/jwambugu/go-mpesa/pkg/config"
 	"gitlab.com/jwambugu/go-mpesa/pkg/mpesa"
 	"log"
+	"net"
+	"net/url"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -29,4 +33,41 @@ func main() {
 	fmt.Println(token)
 
 	fmt.Println(mpesaService.Environment())
+
+	s := strconv.Itoa(254708666389)
+	fmt.Println(s[:3])
+
+	u, err := url.ParseRequestURI("https://127.0.0.1:4040")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	address := net.ParseIP(u.Host)
+
+	log.Println("url-info", "host", address)
+
+	if address == nil {
+		log.Println("url-info", "host", u.Host)
+
+		fmt.Println(strings.Contains(u.Host, "."))
+	}
+
+	fmt.Println(u)
+
+	response, err := mpesaService.LipaNaMpesaOnline(&mpesa.STKPushRequest{
+		Shortcode:     0,
+		Passkey:       "",
+		Amount:        2,
+		PhoneNumber:   0,
+		ReferenceCode: "",
+		CallbackURL:   "",
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(fmt.Printf("%+v", response))
+
 }
